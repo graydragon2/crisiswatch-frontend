f6218ee94c543bbf36045b74b0f6f126b8d9caa3
graydragon2 <52837063+graydragon2@users.noreply.github.com>
2025-07-12 09:25:34 -0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class', // ✅ or 'media' if preferred
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }

afaece92954232bbadd2ee68c789b73e884d4bb8
graydragon2 <52837063+graydragon2@users.noreply.github.com>
2025-07-13 20:55:57 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -16,11 +16,12 @@ export default function App({ Component, pageProps }) {
     <div className="min-h-screen bg-white dark:bg-[#09090b] text-foreground transition-colors">
       <div className="p-4 flex justify-end">
         <button
-          onClick={() => setDark(!dark)}
-          className="px-3 py-1 border rounded-md text-xs"
-        >
-          Toggle {dark ? 'Light' : 'Dark'}
-        </button>
+  onClick={() => setDark(!dark)}
+  className="px-3 py-1 border rounded-md text-xs text-black dark:text-white"
+>
+  Toggle {dark ? 'Light' : 'Dark'}
+</button>
+
       </div>
       <Component {...pageProps} />
     </div>
